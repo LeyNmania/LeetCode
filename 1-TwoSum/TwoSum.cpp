153c0a3af4bc2c5ae80f58acd660e0a83427c7a5
LeyN <leynnoah@gmail.com>
2018-11-15 00:41:01 +0800

    Update TwoSum.cpp

--- 1-TwoSum/TwoSum.cpp
+++ 1-TwoSum/TwoSum.cpp
@@ -14,4 +14,25 @@ public:
         }
         exit(1);
     }
+};   
+// Above 70+ms beats 20%~40%
+// below 4ms beats 99.96%
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
+        vector<int> result;
+        for(int i=0;i<nums.size();i++)
+        {
+            int tmp = target - nums[i];
+            if(hash.find(tmp) != hash.end())
+            {
+                result.push_back(hash[tmp]);
+                result.push_back(i);
+                return result;
+            }
+            hash[nums[i]]=i;
+        }
+        return result;
+    }
 };
