506521e8f083d9579ad120025e706af09103f2be
LeyN <leynnoah@gmail.com>
2018-11-16 00:48:05 +0800

    Update SearchInsertPosition.cpp

--- 35-SearchInsertPosition/SearchInsertPosition.cpp
+++ 35-SearchInsertPosition/SearchInsertPosition.cpp
@@ -28,9 +28,9 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int low = 0, high = nums.size()-1;
 
-        while (low <= high) {
-            int mid = low + (high-low)/2;
-
+        while (low <= high) 
+        {
+            int mid = (high+low)/2;
             if (nums[mid] < target)
                 low = mid+1;
             else
