baa7a9d962e0e36098c40f32a95974caec3b4527
LeyN <leynnoah@gmail.com>
2018-11-15 23:35:32 +0800

    Create BestTimetoBuyandSellStock.cpp

---
+++ 121-BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,19 @@
+//
+// Runtime: 4 ms, faster than 99.51% of C++ online submissions for Best Time to Buy and Sell Stock.
+//
+
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int max=0;
+        int min=INT_MAX;
+        for(int i=0;i<p.size();i++)
+        {
+            if(min>p[i])
+                min=p[i];
+            if(p[i]-min>max)
+                max=p[i]-min;
+        }
+        return max;
+    }
+};
