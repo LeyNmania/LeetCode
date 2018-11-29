619a8048b0b950429590cd9af781dd42a8be9a4e
LeyN <leynnoah@gmail.com>
2018-11-29 20:36:34 +0800

    Create ReverseInteger.cpp

---
+++ 007-ReverseInteger/ReverseInteger.cpp
@@ -0,0 +1,14 @@
+// 12ms beats 97% , considered overflow
+//----------------------
+class Solution {
+public:
+    int reverse(int x) {
+        long res = 0;
+        while(x)
+        {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return (res > INT_MAX || res < INT_MIN)? 0 : res;
+    }
+};
