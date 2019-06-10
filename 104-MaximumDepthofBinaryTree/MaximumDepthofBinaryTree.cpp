c8975b52b9c48f7f19644abc170180e7341b2aee
LeyN <leynnoah@gmail.com>
2019-06-10 09:59:54 +0800

    Update MaximumDepthofBinaryTree.cpp

--- 104-MaximumDepthofBinaryTree/MaximumDepthofBinaryTree.cpp
+++ 104-MaximumDepthofBinaryTree/MaximumDepthofBinaryTree.cpp
@@ -19,3 +19,33 @@ public:
     }
 };
 
+/* BFS is the following
+Runtime: 8 ms, faster than 96.00% of C++ online submissions for Maximum Depth of Binary Tree.
+Memory Usage: 19.4 MB, less than 58.15% of C++ online submissions for Maximum Depth of Binary Tree.
+*/
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        queue<TreeNode*> q;
+        int count = 0; 
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            ++count;
+            for(int i = 0 , n = q.size();i<n;i++)
+            {
+                TreeNode* p = q.front();
+                q.pop();
+                
+                if(p->left)
+                    q.push(p->left);
+                if(p->right)
+                    q.push(p->right);
+            }
+        }
+        return count;
+    }
+};
