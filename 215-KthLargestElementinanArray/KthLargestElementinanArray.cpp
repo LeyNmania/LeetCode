72e0842b70dc7a13b958bc0d6b1465ceec9a0316
周官皓 <leynnoah@gmail.com>
2019-06-28 00:22:19 +0800

    Create KthLargestElementinanArray.cpp
    Partition solution manipulation

---
+++ 215-KthLargestElementinanArray/KthLargestElementinanArray.cpp
@@ -0,0 +1,40 @@
+/* Partition solution without STL method.
+Runtime: 84 ms, faster than 8.48% of C++ online submissions for Kth Largest Element in an Array.
+Memory Usage: 9.2 MB, less than 60.09% of C++ online submissions for Kth Largest Element in an Array.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size()-1, kth;
+        while (true) 
+        {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                kth = nums[idx];
+                break;
+            }
+            if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
+        return kth;
+    }
+private:
+    int partition(vector<int>& nums, int left, int right)
+    {
+        int pivot = nums[left] , l = left + 1, r = right;
+        while(l <= r)
+        {
+            if(nums[l]<pivot && nums[r]>pivot)
+                swap(nums[l++],nums[r--]);
+            if(nums[l]>=pivot)
+                l++;
+            if(nums[r]<=pivot)
+                r--;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
