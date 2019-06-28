f9d4f544a5cec512a54ed2a76fefabdf8c62f1d6
周官皓 <leynnoah@gmail.com>
2019-06-28 08:59:32 +0800

    Update KthLargestElementinanArray.cpp

--- 215-KthLargestElementinanArray/KthLargestElementinanArray.cpp
+++ 215-KthLargestElementinanArray/KthLargestElementinanArray.cpp
@@ -1,4 +1,4 @@
-/* Partition solution without STL method.
+/* Fast Sort with partition function created on my own
 Runtime: 84 ms, faster than 8.48% of C++ online submissions for Kth Largest Element in an Array.
 Memory Usage: 9.2 MB, less than 60.09% of C++ online submissions for Kth Largest Element in an Array.
 */
@@ -36,5 +36,52 @@ private:
         }
         swap(nums[left], nums[r]);
         return r;
+    }
+};
+
+/* Heap sort
+Runtime: 12 ms, faster than 87.41% of C++ online submissions for Kth Largest Element in an Array.
+Memory Usage: 9.1 MB, less than 84.44% of C++ online submissions for Kth Largest Element in an Array.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        buildMaxHeap(nums);
+        for (int i = 0; i < k - 1; i++) {
+            swap(nums[0], nums[--heapSize]);
+            maxHeapify(nums, 0);
+        }
+        return nums[0];
+    }
+private:
+    int heapSize;
+    
+    int left(int i) {
+        return (i << 1) + 1;
+    }
+    
+    int right(int i) {
+        return (i << 1) + 2;
+    }
+    
+    void maxHeapify(vector<int>& nums, int i) {
+        int largest = i, l = left(i), r = right(i);
+        if (l < heapSize && nums[l] > nums[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && nums[r] > nums[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            maxHeapify(nums, largest);
+        }
+    }
+    
+    void buildMaxHeap(vector<int>& nums) {
+        heapSize = nums.size();
+        for (int i = (heapSize >> 1) - 1; i >= 0; i--) {
+            maxHeapify(nums, i);
+        }
     }
 };
