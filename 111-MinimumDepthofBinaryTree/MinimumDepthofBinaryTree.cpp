7adb545790a290bbc0b717fa360fe4ed29a27911
周官皓 <leynnoah@gmail.com>
2019-07-05 11:43:15 +0800

    Update MinimumDepthofBinaryTree.cpp

--- 111-MinimumDepthofBinaryTree/MinimumDepthofBinaryTree.cpp
+++ 111-MinimumDepthofBinaryTree/MinimumDepthofBinaryTree.cpp
@@ -48,7 +48,47 @@ public:
         return left_minDepth > right_minDepth?right_minDepth + 1:left_minDepth + 1;
     }  
 };
+
+/* BFS
+Runtime: 4 ms, faster than 99.92% of C++ online submissions for Minimum Depth of Binary Tree.
+Memory Usage: 19.7 MB, less than 65.82% of C++ online submissions for Minimum Depth of Binary Tree.
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        queue<TreeNode*> Q;
+        Q.push(root);
+        int i = 0;
+        while(!Q.empty())
+        {
+            ++i;
+            int k = Q.size();
+            for(int j=0; j<k; j++)
+            {
+                TreeNode* rt = Q.front();
+                if(rt->left) 
+                    Q.push(rt->left);
+                if(rt->right)
+                    Q.push(rt->right);
+                Q.pop();
+                if(!rt->left && !rt->right)
+                    return i;
+            }
+        }
+        return i;
+    }  
+};
 
     
     
