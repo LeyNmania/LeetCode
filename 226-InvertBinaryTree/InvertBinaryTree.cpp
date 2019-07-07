6c36513bd2cc13abad4d4543d9e3a29ceb1b8dc5
周官皓 <leynnoah@gmail.com>
2019-07-08 00:22:31 +0800

    Update InvertBinaryTree.cpp

--- 226-InvertBinaryTree/InvertBinaryTree.cpp
+++ 226-InvertBinaryTree/InvertBinaryTree.cpp
@@ -1,4 +1,4 @@
-/*
+/* Recursive
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Invert Binary Tree.
 Memory Usage: 9 MB, less than 80.37% of C++ online submissions for Invert Binary Tree.
 */
@@ -21,5 +21,42 @@ public:
             swap(root->left,root->right);
         }
         return root;   
+    }
+};
+
+/* Iterative
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Invert Binary Tree.
+Memory Usage: 9 MB, less than 88.66% of C++ online submissions for Invert Binary Tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode* temp;
+        while(!q.empty())
+        {
+            TreeNode* p = q.front();
+            q.pop();
+            if(p)
+            {
+                q.push(p->left);
+                q.push(p->right);
+                
+                temp = p->left;
+                p->left = p->right;
+                p->right = temp;
+            }
+        }
+        return root;
     }
 };
