c36bacb44d7de6a25fcdcb94ff76c2abea9d3b7e
周官皓 <leynnoah@gmail.com>
2019-07-21 14:28:57 +0800

    Update FindBottomLeftTreeValue.cpp

--- 513-FindBottomLeftTreeValue/FindBottomLeftTreeValue.cpp
+++ 513-FindBottomLeftTreeValue/FindBottomLeftTreeValue.cpp
@@ -33,3 +33,25 @@ private:
         dfs(root->right, depth+1, height, res);
     }
 };
+
+/* BFS
+Runtime: 16 ms, faster than 71.88% of C++ online submissions for Find Bottom Left Tree Value.
+Memory Usage: 20.8 MB, less than 67.72% of C++ online submissions for Find Bottom Left Tree Value.
+*/
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            root = q.front();
+            q.pop();
+            if(root->right != NULL)
+                q.push(root->right);
+            if(root->left != NULL)
+                q.push(root->left);
+        }
+        return root->val;
+    }
+};
